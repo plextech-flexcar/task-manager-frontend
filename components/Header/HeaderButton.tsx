0e02aee7ec955ba1a1e6a6b4f05120f45f00e5d2
Ayaka Chou <chouayaka@gmail.com>
2022-10-21 18:45:34 -0700

    made header and cards responsive, took inline css out (mostly)

---
+++ components/Header/HeaderButton.tsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import { TouchableHighlight, Text, Image, StyleSheet } from "react-native";
+import { styles } from "./styles.js";
+import SvgUri from "react-native-svg-uri";
+
+export default function HeaderButtons(props: { buttonName: string }) {
+  let { buttonName } = props;
+  const [isActive, setIsActive] = useState(false);
+
+  const handleClick = () => {
+    setIsActive(!isActive);
+  };
+
+  return (
+    <>
+      <TouchableHighlight
+        style={isActive ? styles.active : styles.notActive}
+        onPress={handleClick}
+      >
+        <>
+          <Text style={isActive ? styles.activeButton : styles.notActiveButton}>
+            {buttonName}
+          </Text>
+        </>
+      </TouchableHighlight>
+    </>
+  );
+}
--- components/Header/PurpleHeader.tsx
+++ components/Header/PurpleHeader.tsx
@@ -1,63 +1,40 @@
 import React, { useState } from "react";
 import { StyleSheet, Text, View, Image } from "react-native";
-import HeaderButtons from "./HeaderButton";
 import { Searchbar } from "react-native-paper";
+
+import HeaderButtons from "./HeaderButton";
+import { styles } from "./styles";
+
 export default function PurpleHeader() {
   const [searchQuery, setSearchQuery] = React.useState("");
 
   const onChangeSearch = (query: React.SetStateAction<string>) =>
     setSearchQuery(query);
   return (
-    <View
-      style={{
-        height: 148,
-        width: "100%",
-        backgroundColor: "#EDEDED",
-        borderBottomColor: "#d1cdd8",
-        borderBottomWidth: StyleSheet.hairlineWidth,
-        justifyContent: "flex-end",
-        flexDirection: "column",
-      }}
-    >
-      <View
-        style={{
-          width: "100%",
-          justifyContent: "space-between",
-          backgroundColor: "#EDEDED",
-          flexDirection: "row",
-          alignItems: "center",
-          paddingHorizontal: "5px",
-        }}
-      >
+    <View style={styles.headerBack}>
+      <View style={styles.headerSearchRow}>
         <Image
-          source={{ uri: require("../assets/HeaderIcons/user.svg") }}
+          source={{ uri: require("../../assets/HeaderIcons/user.svg") }}
           style={{ width: 16, height: 16 }}
         />
         <Searchbar
-          placeholder="Search Tasks"
+          placeholder="Search tasks"
           placeholderTextColor="#76757D"
           onChangeText={onChangeSearch}
           value={searchQuery}
           style={{
-            width: 200,
+            width: "60%",
             height: 30,
             backgroundColor: "#FFFFFF",
           }}
           inputStyle={{ fontSize: 15 }}
         />
         <Image
-          source={{ uri: require("../assets/HeaderIcons/addlist.svg") }}
+          source={{ uri: require("../../assets/HeaderIcons/addlist.svg") }}
           style={{ width: 16, height: 16 }}
         />
       </View>
-      <View
-        style={{
-          justifyContent: "space-between",
-          flexDirection: "row",
-          alignItems: "center",
-          paddingVertical: "5px",
-        }}
-      >
+      <View style={styles.headerButtonRow}>
         <HeaderButtons buttonName={"Reset "} />
         <Text style={{ color: "#76757D", paddingVertical: 4 }}>50 Tasks</Text>
         <HeaderButtons buttonName={"Sort & Filter"} />
