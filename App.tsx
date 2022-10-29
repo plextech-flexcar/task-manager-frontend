499a670fadb4fce7e1b777d199fb016a4c919992
Thomas Wang <thomaswang@berkeley.edu>
2022-10-28 18:00:50 -0700

    Added Jotai as dependency and added state for all tasks and displayed tasks

--- App.tsx
+++ App.tsx
@@ -13,6 +13,8 @@ import {
   Box,
 } from "native-base";
 import TaskListScreen from "./screens/TaskListScreen/TaskListScreen";
+import { useAtom } from "jotai";
+import { allTasksAtom, displayTasksAtom } from "./atoms";
 
 // Define the config
 const config = {
@@ -28,6 +30,18 @@ declare module "native-base" {
 }
 
 export default function App() {
+  const [allTasks, setAllTasks] = useAtom(allTasksAtom)
+  const [displayTasks, setDisplayTasks] = useAtom(displayTasksAtom)
+
+  fetch("localhost:8080/api/v1/task")
+    .then(response => response.json())
+    .then(data => {
+      setAllTasks(data)
+      setDisplayTasks(data)
+      console.log(allTasks)
+      console.log(displayTasks)
+    })
+
   return (
     <NativeBaseProvider>
           <TaskListScreen/>
