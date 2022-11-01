95b0549fe9baa7f2daf4e914b7993c438457a374
Eugene Chao <46041923+epchao@users.noreply.github.com>
2022-11-01 12:29:23 -0700

    Update App.tsx
    Signed-off-by: Eugene Chao <46041923+epchao@users.noreply.github.com>

--- App.tsx
+++ App.tsx
@@ -34,13 +34,13 @@ export default function App() {
   const [displayTasks, setDisplayTasks] = useAtom(displayTasksAtom)
 
   fetch("localhost:8080/api/v1/task")
-     .then(response => response.json())
-     .then(data => {
-       setAllTasks(data)
-       setDisplayTasks(data)
-       console.log(allTasks)
-       console.log(displayTasks)
-     })
+    .then(response => response.json())
+    .then(data => {
+      setAllTasks(data)
+      setDisplayTasks(data)
+      console.log(allTasks)
+      console.log(displayTasks)
+    })
 
   return (
     <NativeBaseProvider>
