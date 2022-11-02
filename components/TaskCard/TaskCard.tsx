ef094d63cc39dc80d797e4c1a7cc3ee4013a56c4
Eugene Chao <epchao@berkeley.edu>
2022-11-01 20:40:28 -0700

    Fix Parsing error for Super Linter

--- components/TaskCard/TaskCard.tsx
+++ components/TaskCard/TaskCard.tsx
@@ -1,21 +1,15 @@
-import React from "react";
-import { Image } from "react-native";
+import React from 'react';
+import { Image } from 'react-native';
 import {
   Box,
   View,
-  Stack,
   HStack,
   VStack,
-  Heading,
-  Container,
-  Button,
   Text,
-  Icon,
-  Center,
-} from "native-base";
-import { styles } from "./styles.js";
-import { PRIORITY_ICON_MAP } from "./TaskCardPriorityIconMap";
-import { Task } from "../../models/Task.js";
+} from 'native-base';
+import { styles } from './styles.js';
+import { PRIORITY_ICON_MAP } from './TaskCardPriorityIconMap';
+import { Task } from '../../models/Task.js';
 
 const TaskCard = ({
   taskTitle,
@@ -27,27 +21,22 @@ const TaskCard = ({
 }: Task) => {
   return (
     <View style={styles.card}>
-      <Box style={{ width: "100%" }}>
+      <Box style={{ width: '100%' }}>
         <VStack p="4" space={0} width="Fill" height="Hug">
           <HStack justifyContent="space-between">
             <View style={styles.taskHeading}>
-              <Text>
-                {taskTitle}
-              </Text>
+              <Text>{taskTitle}</Text>
             </View>
             <Image
               source={{ uri: PRIORITY_ICON_MAP[priority] }}
-              style={{ width: 20, height: 20 }}/>
+              style={{ width: 20, height: 20 }}
+            />
           </HStack>
           <View style={styles.taskDateText}>
-            <Text>
-              {taskAge} days old
-            </Text>
+            <Text>{taskAge} days old</Text>
           </View>
           <View style={styles.vehicleText}>
-            <Text>
-              {carModel}
-            </Text>
+            <Text>{carModel}</Text>
           </View>
           <HStack justifyContent="space-between">
             <View style={styles.vehicleText}>
@@ -57,7 +46,7 @@ const TaskCard = ({
             </View>
             <View>
               <Image
-                source={{ uri: require("../../assets/Assigned.svg") }}
+                source={{ uri: require('../../assets/Assigned.svg') }}
                 style={{ width: 27, height: 27 }}
               />
             </View>
--- components/TaskCard/TaskCardPriorityIconMap.ts
+++ components/TaskCard/TaskCardPriorityIconMap.ts
@@ -1,6 +1,6 @@
 export const PRIORITY_ICON_MAP: { [key: number]: string } = {
-    1: require("../../assets/priorities/lowPriority.svg"),
-    2: require("../../assets/priorities/normalPriority.svg"),
-    3: require("../../assets/priorities/highPriority.svg"),
-    4: require("../../assets/priorities/topPriority.svg"),
-}
+  1: require('../../assets/priorities/lowPriority.svg'),
+  2: require('../../assets/priorities/normalPriority.svg'),
+  3: require('../../assets/priorities/highPriority.svg'),
+  4: require('../../assets/priorities/topPriority.svg'),
+};
