b1e499c2139b726dfe7a142e6a08e85db863ac10
Taiga Kitao <taiga20022002@gmail.com>
2022-11-04 17:56:27 -0700

    still fixing linter

--- screens/TaskListScreen/TaskListScreen.tsx
+++ screens/TaskListScreen/TaskListScreen.tsx
@@ -30,55 +30,7 @@ const TaskListScreen = () => {
     vin: 'vy6si92Chj',
   };
 
-  const task2: Task = {
-    id: 5,
-    vehicleid: 4,
-    date: 1667185142,
-    type: "Glass Chip: Rear Window Pasenger's side",
-    description:
-      'Using a windshied repair kit, you can remove the broken glass and replace it with a new piece of glass, and clean the area, apply the adhesive.',
-    comment: 'Pls finish ASAP',
-    make: 'Skoda',
-    model: 'Rapid',
-    color: 'Magenta',
-    license: 'qjt7Bi',
-    mva: 'R436542',
-    age: 1667271542,
-    assigned: 'Elias Charambides',
-    market: 'Wisconsin',
-    status: false,
-    createdBy: 'Tom Kitao',
-    carImage:
-      'https://www.freepnglogos.com/uploads/honda-car-png/honda-car-honda-civic-very-good-car-honda-civic-10.png',
-    state: 'NC',
-    vin: 'vy6si92Chj',
-  };
-
-  const task3: Task = {
-    id: 6,
-    vehicleid: 4,
-    date: 1667185142,
-    type: "Glass Chip: Rear Window Pasenger's side",
-    description:
-      'Using a windshied repair kit, you can remove the broken glass and replace it with a new piece of glass, and clean the area, apply the adhesive.',
-    comment: 'Pls finish ASAP',
-    make: 'Skoda',
-    model: 'Rapid',
-    color: 'Magenta',
-    license: 'jt7Bi',
-    mva: 'R436542',
-    age: 1667271542,
-    assigned: 'Elias Charambides',
-    market: 'Wisconsin',
-    status: false,
-    createdBy: 'Jonh Kim',
-    carImage:
-      'https://www.freepnglogos.com/uploads/honda-car-png/honda-car-honda-civic-very-good-car-honda-civic-10.png',
-    state: 'NC',
-    vin: 'vy6si92Chj',
-  };
-
-  const tasks: Task[] = [task1, task2, task3, task1, task2, task3];
+  const tasks: Task[] = [task1];
 
   return (
     <SafeAreaView style={styles.view}>
