05f9141083ce563e9daf3a92f87667012731eb7e
Rhythm <rhythmseth@berkeley.edu>
2022-11-04 18:15:03 -0700

    committing changes to App.tsx

--- App.tsx
+++ App.tsx
@@ -21,14 +21,14 @@ export default function App() {
   const [allTasks, setAllTasks] = useAtom(allTasksAtom);
   const [displayTasks, setDisplayTasks] = useAtom(displayTasksAtom);
 
-  // fetch('localhost:8080/api/v1/task')
-  //   .then((response) => response.json())
-  //   .then((data) => {
-  //     setAllTasks(data);
-  //     setDisplayTasks(data);
-  //     console.log(allTasks);
-  //     console.log(displayTasks);
-  //   });
+  fetch('localhost:8080/api/v1/task')
+    .then((response) => response.json())
+    .then((data) => {
+      setAllTasks(data);
+      setDisplayTasks(data);
+      console.log(allTasks);
+      console.log(displayTasks);
+    });
 
   return (
     <NativeBaseProvider>
