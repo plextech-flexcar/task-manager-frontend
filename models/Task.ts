037bbaff5ce8d4b197155ed7f136c4c63d5970ff
Rhythm <rhythmseth@berkeley.edu>
2022-11-04 18:13:46 -0700

    Trying to implement icon component into taskcard

--- models/Task.ts
+++ models/Task.ts
@@ -1,8 +1,22 @@
 export interface Task {
-  taskTitle: string;
-  taskAge: number;
-  carModel: string;
-  licensePlate: string;
-  mva: number;
+  id: number;
+  vehicleid: number;
+  type: string;
+  date: number;
+  description: string;
+  comment: string;
+  make: string;
+  model: string;
+  color: string;
+  license: string;
+  mva: string;
   priority: number;
+  age: number;
+  assigned: string | null ;
+  market: string;
+  status: boolean;
+  createdBy: string | null;
+  carImage: string;
+  state: string;
+  vin: string;
 }
