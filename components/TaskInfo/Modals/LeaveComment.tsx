21854b732fba8eec4e96113efa24a1b58907a21d
Taiga Kitao <taiga20022002@gmail.com>
2022-11-18 19:05:56 -0800

    finished comment API connection

--- components/TaskInfo/Modals/LeaveComment.tsx
+++ components/TaskInfo/Modals/LeaveComment.tsx
@@ -11,12 +11,36 @@ import {
 import { Modal, FormControl } from 'native-base';
 import { styles } from '../../../screens/TaskInfoScreen/TaskInfoStyles.js';
 import { styles1 } from './leaveCommentStyles';
-const LeaveComment = ({ showModal, onClose }) => {
+
+const LeaveComment = ({
+  showModal,
+  onClose,
+  taskId,
+  postedBy,
+  comments,
+  setComments,
+}) => {
+  const putComment = async (postedBy: String, content: String, taskId: String) => {
+    fetch(`http://localhost:8080/api/v1/newComment`, {
+      mode: 'cors',
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Origin': 'http://localhost:19006',
+      },
+      body: JSON.stringify({ postedBy: postedBy, content: content, taskId: taskId }),
+    })
+      .then((response) => response.json())
+      .then((json) => {
+        setComments([...comments, json]);
+      });
+  };
   const [comment, setComment] = useState('');
   return (
     <View style={styles1.contain}>
       <Modal
-        _backdrop={{bg: "#2A00A5"}}
+        _backdrop={{ bg: '#2A00A5' }}
         isOpen={showModal}
         onClose={() => onClose()}
         safeAreaTop={true}
@@ -73,7 +97,12 @@ const LeaveComment = ({ showModal, onClose }) => {
                   </View>
 
                   <View style={{ flex: 1 }}>
-                    <Pressable onPress={() => onClose()}>
+                    <Pressable
+                      onPress={() => {
+                        onClose();
+                        putComment(postedBy, comment, taskId);
+                      }}
+                    >
                       <Image
                         source={require('../../../assets/submit.png')}
                         style={{ width: 20, height: 20 }}
