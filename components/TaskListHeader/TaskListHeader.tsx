07cb93f52feb05d86b8c75f2bd8db31d26b5de31
Rhythm <rhythmseth@berkeley.edu>
2022-11-30 20:49:10 -0800

    search functionality added - not tested yet

--- components/TaskListHeader/TaskListHeader.tsx
+++ components/TaskListHeader/TaskListHeader.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Text, View, Image } from 'react-native';
 import { Searchbar } from 'react-native-paper';
-import { allTasksAtom } from '../../atoms';
+import { allTasksAtom, displayTasksAtom } from '../../atoms';
+import { allVehiclesAtom } from '../../atoms';
+import { Task } from "../../models/Task";
 import HeaderButtons from './HeaderButton';
 import { styles } from './styles';
 import { useAtom } from 'jotai';
@@ -10,7 +12,41 @@ const addList = require('../../assets/HeaderIcons/addlist.webp');
 export default function TaskListHeader() {
   const [searchQuery, setSearchQuery] = React.useState('');
   const [allTasks] = useAtom(allTasksAtom);
-  const onChangeSearch = (query: React.SetStateAction<string>) => setSearchQuery(query);
+  const [displayTasks, setDisplayTasks] = useAtom(displayTasksAtom);
+  const allVehicles = useAtom(allVehiclesAtom);
+  const onChangeSearch = (query: string) => {
+    setSearchQuery(query)
+    let mva: {[type : number] : string} = {};
+    let license: {[type : number] : string} = {};
+    allVehicles.forEach((value: any, key: number) => {
+      mva[value.getVehicleid() as number] = value.getMva();
+      license[value.getVehicleid() as number] = value.getLicense();
+    });
+    var pattern = new RegExp(query + '[a-zA-Z0-9]*');
+    var matches = [];
+    for (let key in mva) {
+      if (pattern.test(mva[key])) {
+        matches.push(mva[key])
+      }
+    }
+    for (let key in license) {
+      if (pattern.test(license[key])) {
+        matches.push(license[key])
+      }
+    }
+    displayTasks.forEach((task : Task) => {
+      const newDisplayTasks : Task[] = [];
+      if (matches.includes(task.vehicleid)) {
+        newDisplayTasks.push(task);
+      }
+      if (matches.includes(task.vehicleid)) {
+        newDisplayTasks.push(task);
+      }
+      setDisplayTasks(newDisplayTasks);
+    })
+    //logic after u type a single letter
+    //updating displaytasksAtom after implementing the search stuff on displayTasks
+  };
   return (
     <View style={styles.headerBack}>
       <View style={styles.headerSearchRow}>
